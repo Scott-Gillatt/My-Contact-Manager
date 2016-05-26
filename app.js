574c299ad775cf2f294f521466d80823513bae71
Scott Gillatt <scottgillatt@gmail.com>
2016-05-25 19:11:59 -0600

    Using Contact Form

--- app.js
+++ app.js
@@ -1,211 +1,205 @@
 (function () {
 
-    var ContactApp = function () {
+	var ContactApp = function () {
 
-        var self = this;
+		var self = this;
+		
+		this.contacts = [];
+		
+		document.getElementById('addContactOk').addEventListener('click', function () {
 
-        this.contacts = [];
+			var firstName = document.getElementById('addContactFirstName');
+			var lastName = document.getElementById('addContactLastName');
+			var phoneNumber = document.getElementById('addContactPhoneNumber');
+			var address = document.getElementById('addContactAddress');
+			var city = document.getElementById('addContactCity');
+			var state = document.getElementById('addContactState');
+			var zip = document.getElementById('addContactZip');
+			
+			if (firstName.value || lastName.value) {
+				
+				var contact = new Contact(
+					firstName.value,
+					lastName.value,
+					phoneNumber.value,
+					address.value,
+					city.value,
+					state.value,
+					zip.value
+				);
 
-        document.getElementById('addContactOk').addEventListener('click', function () {
+				self.addContactFromTemplate(contact);
+				
+				firstName.value = '';
+				lastName.value = '';
+				phoneNumber.value = '';
 
-            var firstName = document.getElementById('addContactFirstName');
-            var lastName = document.getElementById('addContactLastName');
-            var phoneNumber = document.getElementById('addContactPhoneNumber');
-            var address = document.getElementById('addContactAddress');
-            var city = document.getElementById('addContactCity');
-            var state = document.getElementById('addContactState');
-            var zip = document.getElementById('addContactZip');
-            // document.getElementById('addContactAlert').
-            // if(document.getElementById('addContactAlert').classList('hidden')){
-            // document.getElementById('addContactAlert').classList.remove('hidden');
-            // }
+				$('#addContactModal').modal('hide');
+			}
+			else {
+				self.showAlert('A first name or last name is required.');
+			}
 
-            // listItem.setAttribute('class', 'list-group-item')
+		});
+		
+		document.getElementById('addContactCancel').addEventListener('click', function () {
 
-            if (firstName.value || lastName.value) {
+			document.getElementById('addContactFirstName').value = '';
+			document.getElementById('addContactLastName').value = '';
+			document.getElementById('addContactPhoneNumber').value = '';
+			document.getElementById('addContactAddress').value = '';
+			document.getElementById('addContactCity').value = '';
+			document.getElementById('addContactState').value = '';
+			document.getElementById('addContactZip').value = '';
 
-                var contact = new Contact(
-                    firstName.value,
-                    lastName.value,
-                    phoneNumber.value,
-                    address.value,
-                    city.value,
-                    state.value,
-                    zip.value
-                );
+			self.clearAlert();
 
-                self.addContact(contact);
+			$('#addContactModal').modal('hide');			
+		});
+	};
 
-                firstName.value = '';
-                lastName.value = '';
-                phoneNumber.value = '';
-                address.value = '';
-                city.value = '';
-                state.value = '';
-                zip.value = '';
-                console.log(contact)
-                $('#addContactModal').modal('hide');
-            }
-            else {
-                self.showAlert('A first name or last name is required.');
-            }
+	ContactApp.prototype.showAlert = function (message) {
+		
+		var alert = document.getElementById('addContactAlert');
 
-        });
+		alert.classList.remove('hidden');
+		alert.innerText = message;
+	}
+	
+	ContactApp.prototype.clearAlert = function () {
+		
+		var alert = document.getElementById('addContactAlert');
 
-        document.getElementById('addContactCancel').addEventListener('click', function () {
+		alert.classList.add('hidden');
+		alert.innerText = '';
+	}
+	
+	ContactApp.prototype.addContact = function (contact) {
 
-            document.getElementById('addContactFirstName').value = '';
-            document.getElementById('addContactLastName').value = '';
-            document.getElementById('addContactPhoneNumber').value = '';
-            document.getElementById('addContactAddress').value = '';
-            document.getElementById('addContactCity').value = '';
-            document.getElementById('addContactState').value = '';
-            document.getElementById('addContactZip').value = '';
+		var self = this;
+		
+		this.contacts.push(contact);
 
-            self.clearAlert();
+		var htmlList = document.getElementById('contactList');
+		var html = document.getElementById('contactTemplate').cloneNode(true);
 
-            $('#addContactModal').modal('hide');
-        });
-    };
+		html.setAttribute('id', 'contact-' + contact.id);		
+		html.classList.remove('template');
+		
+		html.getElementsByClassName('contactFullName')[0].innerText = contact.fullName();
+		html.getElementsByClassName('contactFirstName')[0].innerText = contact.firstName;
+		html.getElementsByClassName('contactLastName')[0].innerText = contact.lastName;
+		html.getElementsByClassName('contactPhoneNumber')[0].innerText = contact.phoneNumber;
+		html.getElementsByClassName('contactAddress')[0].innerText = contact.address;
+		html.getElementsByClassName('contactCity')[0].innerText = contact.city;
+		html.getElementsByClassName('contactState')[0].innerText = contact.state;
+		html.getElementsByClassName('contactZip')[0].innerText = contact.zip;
 
-    ContactApp.prototype.showAlert = function (message) {
+		var deleteBtn = html.getElementsByClassName('fa-times')[0].parentNode;
+		
+		deleteBtn.setAttribute('id', contact.id);
 
-        var alert = document.getElementById('addContactAlert');
+		deleteBtn.addEventListener('click', function (event) {
 
-        alert.classList.remove('hidden');
-        alert.innerText = message;
-    }
+			var contactId = parseInt(event.target.parentNode.id);
+			var index = self.contacts.findIndex(function (item) {
+				return item.id === contactId;
+			});
 
-    ContactApp.prototype.clearAlert = function () {
+			self.removeContact(index);
+		});
+		
+		htmlList.appendChild(html);
+	};
+	
+	ContactApp.prototype.addContactFromTemplate = function (contact) {
 
-        var alert = document.getElementById('addContactAlert');
+		var self = this;
+		
+		this.contacts.push(contact);
 
-        alert.classList.add('hidden');
-        alert.innerText = '';
-    }
+		var htmlList = document.getElementById('contactList');
 
-    ContactApp.prototype.addContact = function (contact) {
+		self.getContactTemplate(updateTemplate);
 
-        var self = this;
-        this.contacts.push(contact);
+		function updateTemplate(html) {
 
-        var htmlList = document.getElementById('contactList');
-        var html = document.getElementById('contactTemplate').cloneNode(true);
+			var newDiv = document.createElement('div');
 
-        html.setAttribute('id', 'contact-' + contact.id);
-        html.classList.remove('template');
+			html = html.replace(/\{ id \}/g, contact.id);			
+			html = html.replace(/\{ fullName \}/g, contact.fullName());
+			html = html.replace(/\{ firstName \}/g, contact.firstName);			
+			html = html.replace(/\{ lastName \}/g, contact.lastName);			
+			html = html.replace(/\{ phoneNumber \}/g, contact.phoneNumber);			
+			html = html.replace(/\{ address \}/g, contact.address);			
+			html = html.replace(/\{ city \}/g, contact.city);			
+			html = html.replace(/\{ state \}/g, contact.state);			
+			html = html.replace(/\{ zip \}/g, contact.zip);			
+			
+			newDiv.setAttribute('id', 'contact-' + contact.id);		
+			newDiv.classList.add('contact');
+			newDiv.innerHTML = html;
 
-        html.getElementsByClassName('contactFullName')[0].innerText = contact.fullName();
-        html.getElementsByClassName('contactFirstName')[0].innerText = contact.firstName;
-        html.getElementsByClassName('contactLastName')[0].innerText = contact.lastName;
-        html.getElementsByClassName('contactPhoneNumber')[0].innerText = contact.phoneNumber;
-        html.getElementsByClassName('contactAddress')[0].innerText = contact.address;
-        html.getElementsByClassName('contactCity')[0].innerText = contact.city;
-        html.getElementsByClassName('contactState')[0].innerText = contact.state;
-        html.getElementsByClassName('contactZip')[0].innerText = contact.zip;
+			htmlList.appendChild(newDiv);
+			
+			var deleteBtn = document.getElementById('remove-' + contact.id);
 
-        var deleteBtn = html.getElementsByClassName('fa-times')[0].parentNode
+			deleteBtn.setAttribute('contact-id', contact.id);
 
-        deleteBtn.setAttribute('id', contact.id);
+			deleteBtn.addEventListener('click', function (event) {
 
-        deleteBtn.addEventListener('click', function (event) {
-            var contactID = parseInt(event.target.parentNode.id);
-            var index = self.contacts.findIndex(function (item) {
-                return item.id === contactID;
-            })
-            self.removeContact(index)
-        })
-        htmlList.appendChild(html);
-    };
-    
-    ContactApp.prototype.addContactFromTemplate = function (contact) {
+				var contactId = parseInt(event.currentTarget.getAttributeNode('contact-id').value);
+				var index = self.contacts.findIndex(function (item) {
+					return item.id === contactId;
+				});
 
-        var self = this;
+				self.removeContact(index);
+			});
+		}	
+	};
+	
+	ContactApp.prototype.getContactTemplate = function (callback) {
 
-        this.contacts.push(contact);
+		var httpRequest = new XMLHttpRequest();
 
-        var htmlList = document.getElementById('contactList');
+		httpRequest.addEventListener('load', function () {
+			callback(httpRequest.responseText);
+		});
+		
+		httpRequest.open('GET', '/contactTemplate.html');
+		httpRequest.send();
+	};
+	
+	ContactApp.prototype.removeContact = function (index) {
 
-        self.getContactTemplate(updateTemplate);
+		if (index > -1) {
 
-        function updateTemplate(html) {
+			var contact = this.contacts[index];
 
-            var newDiv = document.createElement('div');
+			var list = document.getElementById('contactList');
+			var html = document.getElementById('contact-' + contact.id);
 
-            html = html.replace(/\{ id \}/g, contact.id);
-            html = html.replace(/\{ fullName \}/g, contact.fullName());
-            html = html.replace(/\{ firstName \}/g, contact.firstName);
-            html = html.replace(/\{ lastName \}/g, contact.lastName);
-            html = html.replace(/\{ phoneNumber \}/g, contact.phoneNumber);
-            html = html.replace(/\{ address \}/g, contact.address);
-            html = html.replace(/\{ city \}/g, contact.city);
-            html = html.replace(/\{ state \}/g, contact.state);
-            html = html.replace(/\{ zip \}/g, contact.zip);
+			list.removeChild(html);
+			
+			this.contacts.splice(index, 1);
+		}	
+	};
 
-            newDiv.setAttribute('id', 'contact-' + contact.id);
-            newDiv.classList.add('contact');
-            newDiv.innerHTML = html;
+	var Contact = function (firstName, lastName, phoneNumber, address, city, state, zip) {
+		this.id = new Date().getTime();
+		this.firstName = firstName || "Scott";
+		this.lastName = lastName || "Gillatt";
+		this.phoneNumber = phoneNumber || "208-888-8888";
+		this.address = address || "Some cool Place Ave";
+		this.city = city || "Nampa";
+		this.state = state || "Idaho";
+		this.zip = zip || 83686;
+	};
 
-            htmlList.appendChild(newDiv);
+	Contact.prototype.fullName = function () {
+		return this.firstName + ' ' + this.lastName;
+	};
 
-            var deleteBtn = document.getElementById('remove-' + contact.id);
-
-            deleteBtn.setAttribute('contact-id', contact.id);
-
-            deleteBtn.addEventListener('click', function (event) {
-
-                var contactId = parseInt(event.currentTarget.getAttributeNode('contact-id').value);
-                var index = self.contacts.findIndex(function (item) {
-                    return item.id === contactId;
-                });
-
-                self.removeContact(index);
-            });
-        }
-    };
-
-    ContactApp.prototype.getContactTemplate = function (callback) {
-
-        var httpRequest = new XMLHttpRequest();
-
-        httpRequest.addEventListener('load', function () {
-            callback(httpRequest.responseText);
-        });
-
-        httpRequest.open('GET', '/contactTemplate.html');
-        httpRequest.send();
-    };
-
-    ContactApp.prototype.removeContact = function (index) {
-        if (index > -1) {
-            var contact = this.contacts[index]
-            var list = document.getElementById('conactList');
-            var html = document.getElementById('contact-' + contact.id);
-            list.removeChild(html);
-            this.contacts.splice(index, 1);
-        }
-    };
-
-    var Contact = function (firstName, lastName, phoneNumber, address, city, state, zip) {
-        this.id = new Date().getTime();
-        this.firstName = firstName || "Scottie";
-        this.lastName = lastName || "Giblet";
-        this.phoneNumber = phoneNumber || "208-208-2088";
-        this.address = address || "Somewhere Fun";
-        this.city = city || "Nampa";
-        this.state = state || "ID";
-        this.zip = zip || 83686;
-    };
-
-    Contact.prototype.fullName = function () {
-        return this.firstName + ' ' + this.lastName;
-    };
-
-    var app = new ContactApp();
+	var app = new ContactApp();
 
 })();
-
-// Asynchronous  (Non Blocking)
-// JavaScript
-// And
-// Xml
